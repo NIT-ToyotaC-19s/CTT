ee00bd5817d88daef75d13e4693384b169d315f9
Chaha1n <chahaaaaan@gmail.com>
2020-08-25 12:20:28 +0900

    なんか二重になってるとこがあったので直した

--- index.js
+++ index.js
@@ -2,16 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'static')));
-
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const path = require('path');
 const fs = require('fs');
-const { resolveAny } = require('dns');
+
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.json());
