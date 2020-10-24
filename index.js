f791da4c6781a31af9f764d8eaa0b418ada2dc22
Chaha1n <chahaaaaan@gmail.com>
2020-10-24 16:36:57 +0900

    install connect-history-api-fallback

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
 const path = require('path');
 const fs = require('fs');
+var history = require('connect-history-api-fallback');
 
-
+app.use(history());
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 
 app.post('/api', (req, res) => {
