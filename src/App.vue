7f12928d5f99b1d00004057cbd65083aa8fbe3ca
nono-n1I <nono6217n@gmail.com>
2020-10-30 10:48:40 +0900

    Remove a bag (issue/18)

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,13 @@
 <template>
-  <div id="app">
-    <!-- <v-app-bar
-      app
-      color="gray"
-      dark
-    > -->
-      <!-- <div class="d-flex align-center"> -->
-        <!-- <h1>Capture The Toyota</h1> -->
-        <!-- <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        /> -->
-
-        <!-- <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        /> -->
-      <!-- </div> -->
-
-      <!-- <v-spacer></v-spacer> -->
-
-      <!-- <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn> -->
-    <!-- </v-app-bar> -->
-
-    <!-- <v-main> -->
-      <!-- <router-link to="/">home</router-link> -->
-      <!-- <router-view /> -->
-      <!-- <router-link to="/catalog">catalog</router-link>,<router-view /> -->
-      <!-- <router-link to="/question">question</router-link> -->
-      <!-- <router-link :to="{name: 'Catalog', query: {id: 1}}">catalog</router-link>, -->
-      <!-- <router-link :to="{name: 'Question', query: {id: 1}}">question</router-link>
-      <router-view /> -->
-    <!-- </v-main> -->
-
-
-
+  <!-- <div id="app"> -->
+  <v-app>
 
   <v-card
-    class="mx-auto overflow-hidden"
+    class="overflow-hidden"
     style="margin-top:60px"
   >
     <v-app-bar
-      color="deep-purple"
+      color="white"
       light
       fixed
     >
@@ -76,7 +27,7 @@
       >
         <v-list-item-group
           v-model="group"
-          active-class="deep-purple--text text--accent-4"
+          active-class="primary--text text--accent-4"
         >
 
           <v-list-item v-for="list in lists" :key="list.name" :to="list.link">
@@ -91,7 +42,8 @@
     </v-navigation-drawer>
     <router-view/>
   </v-card>
-</div>
+<!-- </div> -->
+</v-app>
 </template>
 
 <script>
